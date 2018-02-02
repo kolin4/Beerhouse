bdc589ac5fc057db462b85c3d5622d821b2e2b94
Krzysztof Kolacz <kolin4@gmail.com>
2018-02-02 13:54:57 +0100

    fixed landscape view

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers,applyMiddleware } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 // importing reducers
 import headerReducer from './store/reducers/headerReducer';
